bf868af0b09624d25dc11a678078754cb26b57b3
YolradeeIT <64070089@kmitl.ac.th>
2021-10-16 14:29:10 +0700

    fixed no kuay

--- main.py
+++ main.py
@@ -44,26 +44,31 @@ async def on_message(message):
         print(message.channel)
         await message.channel.send("ฝันดีค่า")
         await client.logout()
-    elif 'ต่อยกับ' in message.content:
+    elif message.content.startswith('ต่อยกับ'):
         print(message.channel)
         result = randint(1,3)
         if result == 1:
-            await message.channel.send(message.author.mention + " ต่อยชนะไอ " + str(randint(1, 50)) + " แต้ม")
+            await message.channel.send(message.author.mention + " ต่อยชนะ" + message.content[7:] + " " + str(randint(1, 50)) + " แต้ม")
         elif result == 2:
-            await message.channel.send("ไอต่อยชนะ "+ message.author.mention + " " + str(randint(1, 50)) + " แต้ม")
+            await message.channel.send(message.content[7:] + "ต่อยชนะ "+ message.author.mention + " " + str(randint(1, 50)) + " แต้ม")
         elif result == 3:
-            await message.channel.send("ต่อยเสมอกับไอ")
+            await message.channel.send(message.author.mention + "ต่อยเสมอกับ" + message.content[7:])
+    elif message.content.startswith('กอดกับ'):
+        print(message.channel)
+        result = randint(1,2)
+        if result == 1:
+            await message.channel.send(message.content[6:] + "กอดกลับ")
+        elif result == 2:
+            await message.channel.send(message.content[6:] + "หันหน้าหนี")
     elif "นอน" in message.content:
         print(message.channel)
         await message.channel.send("ฝันดีนะคะ")
     elif "นิว" in message.content:
         print(message.channel)
         await message.channel.send(f"<@{new}> สุดตีน")
-    elif "ควย" in message.content:
-        print(message.channel)
-        await message.channel.send("~~ควย~~ กระจู๋")
-        await client.logout()
-        client.run(discord_token)
+    # elif "ควย" in message.content:
+    #     print(message.channel)
+    #     await message.channel.send("~~ควย~~ กระจู๋")
     elif "ปัน" in message.content:
         print(message.channel)
         await message.channel.send(f"<@{pun}> ติดแก้ม")
@@ -72,13 +77,6 @@ async def on_message(message):
     elif message.content == "Ai คือ":
         print(message.channel)
         await message.channel.send("Ai คือ Bot ชื่อ Ai ยินดีที่ได้รู้จักนะคะ")
-    elif message.content == "กอดกับไอ":
-        print(message.channel)
-        result = randint(1,2)
-        if result == 1:
-            await message.channel.send("ไอกอดกลับ")
-        elif result == 2:
-            await message.channel.send("ไอหันหน้าหนี")
     elif message.content == "ทำยังไงถึงจะชนะไอ":
         print(message.channel)
         await message.channel.send("ไม่มีทางหรอกค่ะ เพราะคุณอ่อนเกินไป")
